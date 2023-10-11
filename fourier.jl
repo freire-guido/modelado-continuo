5c03794221271380029518ef8f47fb6c139f2592
roccoditella <roccoditella1@gmail.com>
2023-10-11 17:08:17 -0300

    Yellow submarine

--- fourier.jl
+++ fourier.jl
@@ -7,15 +7,6 @@ using InteractiveUtils
 # ╔═╡ 3383a396-6834-11ee-11cc-67867b504477
 using WAV
 
-# ╔═╡ c881b4aa-b32a-4bc5-b6e0-96976dc1df3e
-begin
-	fs = 44.1e3
-	w = 200
-	t = 0:1/fs:1
-	y = 0.1sin.(2pi*w*t)
-	wavplay(y, fs)
-end
-
 # ╔═╡ 3d8fd993-21a6-463c-ab7f-87ea76c7974d
 function create_wave(freq,fs;time=1)
 	t = 0:1/fs:time
@@ -24,26 +15,38 @@ function create_wave(freq,fs;time=1)
 end
 
 # ╔═╡ 46455c19-5a49-43a3-a805-a31e3f5fb449
-function vec2wave(vec, ts = 1, fs = 44100)
+function vec2wave(vec, ts = nothing, fs = 44100)
 	y = zeros(0)
-	ws = 440*(2 .^(2 .*vec./12))
+	if ts == nothing
+		ts = ones(length(vec))
+	end
+	ws = 440*(2 .^(vec./12))
 	for i in 1:length(vec)
 		append!(y, create_wave(ws[i],fs,time = ts[i]))
 	end
 	return y
 end
 
+# ╔═╡ 7d0c9b3d-27f2-43ea-8810-eb0efef17e2a
+begin
+	La_ = 0
+	Si = 2
+	Do = Si + 1
+	Re = Do + 2
+	Mi = Re + 2
+	Fa = Mi + 1
+	Sol = Fa + 2
+	La = Sol + 2
+end
+
 # ╔═╡ a7419f87-de33-45c5-98f6-7956740f882f
 begin
-
-vec = [3,4,5,3,2,3,1].+ 2
-ts  = [1,1,2,1,1,1,2]./2
+fs = 44100
+vec = [Mi,Fa,Sol,Mi,Re,Mi,Do,Mi,Mi,Re,Do,La_,La_,Mi,Mi,Re]
+ts  = [1,1,5,1,1,1,6,1,1,1,1,3,1,1,1,6]./3
 	
 wavplay(vec2wave(vec, ts, fs), fs)
 end
-
-# ╔═╡ 943844d6-0c78-4488-adca-42c6b2331e13
-zeros(0)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -58,11 +61,13 @@ WAV = "~1.2.0"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.2"
+julia_version = "1.9.3"
 manifest_format = "2.0"
+project_hash = "d43127297c462ffb6150506313fe6dee773f9fa9"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -75,14 +80,18 @@ deps = ["Printf"]
 uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
 
 [[deps.Downloads]]
-deps = ["ArgTools", "LibCURL", "NetworkOptions"]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
 
 [[deps.FileIO]]
 deps = ["Pkg", "Requires", "UUIDs"]
 git-tree-sha1 = "299dc33549f68299137e51e6d49a13b5b1da9673"
 uuid = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549"
 version = "1.16.1"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
 
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
@@ -91,10 +100,12 @@ uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -103,6 +114,7 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -117,16 +129,20 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.2+0"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2022.10.11"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
 
 [[deps.Pkg]]
-deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.9.2"
 
 [[deps.Printf]]
 deps = ["Unicode"]
@@ -148,6 +164,7 @@ version = "1.3.0"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -158,10 +175,12 @@ uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
 
 [[deps.UUIDs]]
 deps = ["Random", "SHA"]
@@ -179,22 +198,24 @@ version = "1.2.0"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.13+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
 # ╠═3383a396-6834-11ee-11cc-67867b504477
-# ╠═c881b4aa-b32a-4bc5-b6e0-96976dc1df3e
 # ╠═3d8fd993-21a6-463c-ab7f-87ea76c7974d
 # ╠═46455c19-5a49-43a3-a805-a31e3f5fb449
+# ╠═7d0c9b3d-27f2-43ea-8810-eb0efef17e2a
 # ╠═a7419f87-de33-45c5-98f6-7956740f882f
-# ╠═943844d6-0c78-4488-adca-42c6b2331e13
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
